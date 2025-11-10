b030a9dc974fdd5f3975c1371b4605fc68a9cf16
shawnye2000 <158484842+shawnye2000@users.noreply.github.com>
2025-11-10 12:48:06 +0800

    Add files via upload

---
+++ recommender/model/__init__.py
@@ -0,0 +1,8 @@
+from .base import BaseModel
+from .random import Random
+from .pop import Pop
+from .mf import MF
+from .DIN import DIN
+from .DCN import DCN
+from .bpr import BPR
+__all__ = ["BaseModel", "Random", "MF", "DIN", "DCN", "BPR", "Pop"]
